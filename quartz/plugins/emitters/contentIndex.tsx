ea03472c38bdb0d4d1eb1478386a4be7c2f64dec
MustardDay <mustardday@proton.me>
2025-11-25 02:42:12 -0500

    Filter for Rss
    atte,pted at least

--- quartz/plugins/emitters/contentIndex.tsx
+++ quartz/plugins/emitters/contentIndex.tsx
@@ -74,7 +74,10 @@ function generateRSSFeed(cfg: GlobalConfiguration, idx: ContentIndexMap, limit?:
 
       return f1.title.localeCompare(f2.title)
     })
-    .map(([slug, content]) => createURLEntry(simplifySlug(slug), content))
+       .filter(([slug, _content]) =>
+  slug.startsWith("content/Captain's Log (RSS)"))
+    .map(([slug, content]) => 
+  createURLEntry(simplifySlug(slug), content))
     .slice(0, limit ?? idx.size)
     .join("")
 
