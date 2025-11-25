040f88e416dc09666871dec18e4a597d872cca12
MustardDay <mustardday@proton.me>
2025-11-25 09:51:31 -0500

    Update contentIndex.tsx

--- quartz/plugins/emitters/contentIndex.tsx
+++ quartz/plugins/emitters/contentIndex.tsx
@@ -75,8 +75,10 @@ function generateRSSFeed(cfg: GlobalConfiguration, idx: ContentIndexMap, limit?:
       return f1.title.localeCompare(f2.title)
     })
           
-       .filter(([slug, _content]) => slug.endsWith("(RSS)"))
+   ts
+    .filter(([slug, _content]) => slug.endsWith("(RSS)"))
     .map(([slug, content]) => createURLEntry(simplifySlug(slug), content))
+
     .slice(0, limit ?? idx.size)
     .join("")
 
