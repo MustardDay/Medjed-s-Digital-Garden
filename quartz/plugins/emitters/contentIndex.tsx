f11fced09b0a279f0c5eac3d5f6e8661af962096
MustardDay <mustardday@proton.me>
2025-11-25 03:06:50 -0500

    change filter to be less narrow

--- quartz/plugins/emitters/contentIndex.tsx
+++ quartz/plugins/emitters/contentIndex.tsx
@@ -74,10 +74,10 @@ function generateRSSFeed(cfg: GlobalConfiguration, idx: ContentIndexMap, limit?:
 
       return f1.title.localeCompare(f2.title)
     })
-       .filter(([slug, _content]) =>
-  slug.startsWith("Captain's Log (RSS)"))
+           .filter(([slug, _content]) => 
+             slug.endsWith("(RSS)")))
     .map(([slug, content]) => 
-  createURLEntry(simplifySlug(slug), content))
+      createURLEntry(simplifySlug(slug), content))
     .slice(0, limit ?? idx.size)
     .join("")
 
