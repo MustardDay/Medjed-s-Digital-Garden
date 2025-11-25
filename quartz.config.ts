6067bdde56cb5b80804dfa18912560b6fc757871
MustardDay <mustardday@proton.me>
2025-11-25 14:52:27 -0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "medjedsdigitalgarden.nekoweb.org",
+    baseUrl: "https://mustarddayart.nekoweb.org/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
