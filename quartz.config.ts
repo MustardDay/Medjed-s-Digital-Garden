b148b329231386618e5246977c18a1dbe17d7f7c
MustardDay <mustardday@proton.me>
2025-12-03 00:52:41 -0500

    Update quartz.config.ts changed body text font and darkened

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Kablammo",
-        body: "Marck Script",
+        body: "Beth Ellen",
         code: "IBM Plex Mono",
       },
       colors: {
